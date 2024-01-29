efdb611a63262ec25d560326a370ec5bdb4a9206
costalima10 <gabrelcostalima10@gmail.com>
2024-01-29 12:02:20 -0300

    adicionando postagem

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-ml7a.onrender.com'
+  baseURL: 'https://db-blogpesoal-clvr.onrender.com/'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
