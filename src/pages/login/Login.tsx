341b4f6f9c8e95bbd332f14ae8f9a610ebb164bb
costalima10 <gabrelcostalima10@gmail.com>
2024-01-31 09:29:46 -0300

    alterando importaçao

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
-import './Login.css';
+import './Login.css'; // esta linha foi alterada, a importaçao esta como letra minuscula no inicio 
 
 import { Link, useNavigate } from 'react-router-dom';
 
